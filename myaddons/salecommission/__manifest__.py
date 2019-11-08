da88c786e811c47cfb8f71e49e682dc5161b9cfa
Giang Pham <giangpham.1751996@gmail.com>
2019-11-08 10:06:50 +0700

    Update __manifest__.py

--- myaddons/salecommission/__manifest__.py
+++ myaddons/salecommission/__manifest__.py
@@ -20,18 +20,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product',],
+    'depends': ['base', 'product', ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+
+        'views/product_template_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
-
-        'views/product_template_view.xml',
     ],
 }
